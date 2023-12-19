838a59a8d33e2e8e39a461ea2e1e0a61613eb5d8
baszanowskijakub <baszanowskijakub@gmail.com>
2023-12-19 18:58:17 +0100

    Nato Alphabet update
    The NATO alphabet project has been updated to include a KeyWord error exception.

--- Day 30/main.py
+++ Day 30/main.py
@@ -1,19 +1,7 @@
-# FileNotFound
-try:
-    file = open("a_file.txt")
-except:
-    file = open("a_file", "w")
-    file.write("Something")
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+if height > 3:
+    raise ValueError("Huma height should not be over 3m.")
 
-# KeyError
-# a_dictionary = {"key":"value"}
-# value = a_dictionary["faasafs"]
-
-# IndexError
-# fruit_list = ["Apple", "Banana", "Pear"]
-# fruit = fruit_list[3]
-
-# TypeError
-# text = "abc"
-# print(text + 5)
-
+bmi = weight / height * 2
+print(bmi)
