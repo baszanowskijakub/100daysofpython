d5085245d1bba33f1afced8612c998b97d06aeb4
baszanowskijakub <baszanowskijakub@gmail.com>
2024-01-14 16:36:53 +0100

    Day 45 upload

---
+++ Day 45 - Web Scraping, movie list/main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+soup = BeautifulSoup(response.text, "html.parser")
+with open("./movies.txt", "w", encoding="utf-8") as movie_list:
+    article_titles = []
+    titles = soup.findAll(name="h3", class_="title")
+    for title in titles:
+        article_titles.append(title.getText())
+    article_titles.reverse()
+    for movie in article_titles:
+        movie_list.write(movie + f"\n")
+
