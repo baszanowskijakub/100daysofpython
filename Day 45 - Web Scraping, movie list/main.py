89cdab0428419bc3c3e6ba7d6d8af01a49dcdb03
baszanowskijakub <baszanowskijakub@gmail.com>
2024-01-15 13:21:14 +0100

    Day 45 update

--- Day 45 - Web Scraping, movie list/main.py
+++ Day 45 - Web Scraping, movie list/main.py
@@ -3,6 +3,7 @@ import requests
 
 response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 soup = BeautifulSoup(response.text, "html.parser")
+
 with open("./movies.txt", "w", encoding="utf-8") as movie_list:
     article_titles = []
     titles = soup.findAll(name="h3", class_="title")
